146969f91d4ba62b3c60a853955ca11f47fa8ab3
kafyar2022 <kafyar2022@gmail.com>
2022-07-13 16:10:37 +0500

    Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  PROMO_FILM: {
+    title: 'The Grand Budapest Hotel',
+    genre: 'Drama',
+    releaseDate: '2014',
+  },
+  FILMS_COUNT: 20,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      promoFilm={Setting.PROMO_FILM}
+      filmsCount={Setting.FILMS_COUNT}
+    />
   </React.StrictMode>,
 );
