914519304dd9c26c30d46bf5bfe956b9a7bb99f5
kafyar2022 <kafyar2022@gmail.com>
2022-07-16 12:57:59 +0500

    Маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import FilmScreen from '../../pages/film-screen/film-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import NotFoundScreen from '../../pages/not-found/not-found';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type PromoFilm = {
   title: string;
@@ -11,12 +20,46 @@ type AppScreenProps = {
   filmsCount: number;
 }
 
-function App({promoFilm, filmsCount}: AppScreenProps): JSX.Element {
+function App({ promoFilm, filmsCount }: AppScreenProps): JSX.Element {
   return (
-    <MainScreen
-      promoFilm={promoFilm}
-      filmsCount={filmsCount}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen promoFilm={promoFilm} filmsCount={filmsCount} />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignInScreen />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <MyListScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<FilmScreen />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReviewScreen />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
